d76827155d211211839b1afc011ec7d94895c229
luce096 <luce096@naver.com>
2023-05-01 00:11:38 +0900

    User Server Work Start

--- NodeBase/src/main.ts
+++ NodeBase/src/main.ts
@@ -1,18 +1,13 @@
 
 import Config from "../Config";
 import UserLoader from "./ServerLoader/Target/User";
-import AdminLoader from "./ServerLoader/Target/Admin";
-import DFSLoader from "./ServerLoader/Target/DFS";
-import DBLoader from "./ServerLoader/Target/MariaDB";
-import Logger from "./Module/Logger";
+import Logger from "./modules/Logger"
 
 (async function () {
 
     // 고객
     if (["USER"].indexOf(Config.SERVER_TYPE) >= 0) {
         Logger.info(Config.SERVER_TYPE + ' Is Loading')
-        Logger.info('DataBase is Loading')
-        await DBLoader();
         await UserLoader();
     }
 
